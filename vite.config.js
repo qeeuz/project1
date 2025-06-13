3dd94c86851f1c5bbd708f05f7b2410e4400164e
Shahzad <shahzad.shaikh@student.rocva.nl>
2025-06-13 16:15:47 +0200

    as

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   server: {
     port: 3000,
-    host: true
-  }
-})
+    host: true,
+  },
+  base: "./",
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "offerte.html",
+          dest: ".", // zet het in dist/
+        },
+      ],
+    }),
+  ],
+});
